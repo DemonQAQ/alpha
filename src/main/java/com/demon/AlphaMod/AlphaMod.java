34a99ed3668457a58a4217243a0dbcdef857c9d9
Demon <598044692@qq.com>
2021-10-20 22:24:00 +0800

    Initial commit

---
+++ src/main/java/com/demon/AlphaMod/AlphaMod.java
@@ -0,0 +1,31 @@
+package com.demon.AlphaMod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = AlphaMod.MODID, name = AlphaMod.NAME, version = AlphaMod.VERSION)
+public class AlphaMod
+{
+    public static final String MODID = "alphamod";
+    public static final String NAME = "Alpha Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
